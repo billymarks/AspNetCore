51568ece864201afaec1a2c67cf98dcd05013319
Sawa <fourstar.an@gmail.com>
2017-06-04 22:26:04 +0300

    Добавлены модели, подключена база данных.

---
+++ MobileStore/Migrations/MobileContextModelSnapshot.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MobileStore.Models;
+
+namespace MobileStore.Migrations
+{
+    [DbContext(typeof(MobileContext))]
+    partial class MobileContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("MobileStore.Models.Order", b =>
+                {
+                    b.Property<int>("OrderId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Address");
+
+                    b.Property<string>("ContactPhone");
+
+                    b.Property<int>("PhoneId");
+
+                    b.Property<string>("User");
+
+                    b.HasKey("OrderId");
+
+                    b.HasIndex("PhoneId");
+
+                    b.ToTable("Orders");
+                });
+
+            modelBuilder.Entity("MobileStore.Models.Phone", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Company");
+
+                    b.Property<string>("Name");
+
+                    b.Property<int>("Price");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Phones");
+                });
+
+            modelBuilder.Entity("MobileStore.Models.Order", b =>
+                {
+                    b.HasOne("MobileStore.Models.Phone", "Phone")
+                        .WithMany()
+                        .HasForeignKey("PhoneId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
